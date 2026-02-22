e73d243a3b9635cd00005e42683547af3957b9a9
semana-divin <semanadivin@gmail.com>
2026-02-22 23:33:55 +0200

    feature

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,26 +16,32 @@ if __name__ == "__main__":
 
     employee_id = sys.argv[1]
 
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-        employee_id
+    user_url = (
+        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
     )
-    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        employee_id
+    todos_url = (
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            employee_id
+        )
     )
 
     user_response = requests.get(user_url)
     todos_response = requests.get(todos_url)
 
-    if user_response.status_code != 200:
+    if user_response.status_code != 200 or todos_response.status_code != 200:
         sys.exit(1)
 
     user = user_response.json()
     todos = todos_response.json()
 
-    employee_name = user["name"]
-
+    employee_name = user.get("name")
     total_tasks = len(todos)
-    done_tasks = [task for task in todos if task["completed"] is True]
+
+    done_tasks = []
+    for task in todos:
+        if task.get("completed") is True:
+            done_tasks.append(task)
+
     number_of_done_tasks = len(done_tasks)
 
     print(
@@ -47,4 +53,4 @@ if __name__ == "__main__":
     )
 
     for task in done_tasks:
-        print("\t {}".format(task["title"]))
+        print("\t{}".format(task.get("title")))
